cb16d35bdf154b61d9f21d83f8ad18195f4139fd
misha slavin <mishaslavin@gmail.com>
2012-08-29 02:15:58 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chompion::Application.config.secret_token = 'd4d475276afd6824dd8d3a378dd928b921a6844995f3051889c67ac747861701e4f69af0af2f93ac4593dd87ac23b33342d0d857800cea452dadad32a50f0504'
